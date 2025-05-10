e39336e21e1aaf2058b34771db827d6d96bc8c38
Kyle Gillett <kgillett5@outlook.com>
2025-05-10 16:54:52 +0100

    Homepage change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed again
+          This is the best version now lol
         </p>
         <a
           className="App-link"
